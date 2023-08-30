bd08716a3f336e52981a0efbd9409a0614197f1e
tznl <napkin@tuta.io>
2023-08-30 14:47:23 -0500

    style(9) compliant again

--- include/response.h
+++ include/response.h
@@ -6,15 +6,14 @@ enum cards_columns {C_ID, C_NAME, C_DESCRIPTION, C_QUANTITY, C_IMAGE_LINK};
 enum cards_relation {R_USER_ID, R_CARD_ID}; 
 
 void simple_embed_string
-	(struct discord*, const struct discord_interaction*, char*);
+(struct discord*, const struct discord_interaction*, char*);
+
 void claim_success_embed
-	(struct discord*, const struct discord_interaction*, MYSQL_ROW);
+(struct discord*, const struct discord_interaction*, MYSQL_ROW);
 
 void spawncard_success_embed                                                     
-	(struct discord* client, const struct discord_interaction* event,
-	MYSQL_ROW row, float rarity);
+(struct discord*, const struct discord_interaction*, MYSQL_ROW, float);
 
 void inventory_embed                                                     
-	(struct discord* client, const struct discord_interaction* event,
-	MYSQL_ROW row, float rarity, int type);
+(struct discord*, const struct discord_interaction*, MYSQL_ROW, float, int);
 #endif
